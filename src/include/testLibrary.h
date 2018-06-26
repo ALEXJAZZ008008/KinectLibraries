ef085217dcb75c7f46a5d659217c048669d47abe
Nikos Efthimiou <nikos.efthimiou@gmail.com>
2018-06-26 21:15:56 +0100

    initial test_pthread stuff

--- src/include/testLibrary.h
+++ src/include/testLibrary.h
@@ -1,40 +1,45 @@
 #include <iostream>
 
-
+//!
+//! \class
+//! \brief The TestLibrary class
+//! \details Abstract parent class for all test classes.
 class TestLibrary
 {
-public: 
+public:
     //! Default constructor
-  explicit TestLibrary();
-  
-  //! Destructor, outputs a diagnostic message
-  virtual ~TestLibrary();
+    explicit TestLibrary();
 
-  //! Function (to be overloaded) which does the actual tests.
-  /*! This function is expected to do a series of calls to check(), check_if_equal() etc. */
-  virtual void run_tests() = 0;
-  
-  //! Returns if all checks were fine upto now.
-  bool is_everything_ok() const;
+    //! Destructor, outputs a diagnostic message
+    virtual ~TestLibrary();
 
-  int get_result() const;
+    //! Function (to be overloaded) which does the actual tests.
+    /*! This function is expected to do a series of calls to check(), check_if_equal() etc. */
+    virtual void run_tests() = 0;
 
- private:
-  bool everything_ok;
-    
+    //! \returns if all checks were fine upto now.
+    bool is_everything_ok() const;
+
+    //! Final return to the system. \return EXIT_SUCCESS or EXIT_FAILURE
+    int get_result() const;
+
+private:
+    //! True if everything_ok
+    bool everything_ok;
+
 };
-    
+
 TestLibrary::TestLibrary():
-everything_ok(true)
+    everything_ok(true)
 {}
 
 TestLibrary::~TestLibrary()
 {
 
-  if (everything_ok)
-    std::cerr << "\nAll tests ok !\n\n" << std::endl;
-  else
-    std::cerr << "\nEnd of tests. Please correct errors !\n\n" << std::endl;
+    if (everything_ok)
+        std::cerr << "\nAll tests ok !\n\n" << std::endl;
+    else
+        std::cerr << "\nEnd of tests. Please correct errors !\n\n" << std::endl;
 }
 
 bool TestLibrary::is_everything_ok() const
