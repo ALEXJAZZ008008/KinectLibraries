68de2904bfdd4ba0feaca23232058b98d0d3e5a2
Nikos Efthimiou <nikos.efthimiou@gmail.com>
2018-06-26 02:08:34 +0100

    Added the template for test_pthread

--- src/test/test_pthread.cxx
+++ src/test/test_pthread.cxx
@@ -1,1 +1,29 @@
- 
+#include "testLibrary.h"
+
+class TestPThread : public TestLibrary
+{
+public:
+   explicit TestPThread();
+
+    void run_tests();
+
+};
+
+TestPThread::TestPThread()
+    :TestLibrary()
+{
+
+}
+
+void TestPThread::run_tests()
+{
+
+}
+
+int main()
+{
+  TestPThread test;
+  test.run_tests();
+  return test.get_result();
+}
+
